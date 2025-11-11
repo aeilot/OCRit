26ec0e39331c747cb578d631c865fb6ea2ebfee7
Louis Deng <louisaeilotd@gmail.com>
2025-11-11 12:51:44 +0800

    Initial Commit

---
+++ OCRit/OCRitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCRitApp.swift
+//  OCRit
+//
+//  Created by Chenluo Deng on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct OCRitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
